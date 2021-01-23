c7a18f43f15bdaf8b801b23b493d052cc3f941bd
Crazy Ryan <>
2021-01-23 19:01:26 +0800

    [RanWang] feat: add the exceptions for all failed scenarios for the login endpoint

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/config/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/config/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package com.thoughtworks.capacity.gtb.mvc.config;
 
 import java.util.Objects;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -27,9 +32,19 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(UsernamePasswordMismatchException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
     public ErrorResult handle(UsernamePasswordMismatchException ex) {
-        return new ErrorResult(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return new ErrorResult(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ErrorResult handle(ConstraintViolationException ex) {
+        return new ErrorResult(HttpStatus.BAD_REQUEST.value(),
+                ex.getConstraintViolations().stream().findFirst().
+                        orElseThrow(()->new ResponseStatusException(HttpStatus.BAD_REQUEST, "bad response"))
+                        .getMessage());
     }
 }
--- src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
@@ -4,7 +4,9 @@ import com.thoughtworks.capacity.gtb.mvc.model.User;
 import com.thoughtworks.capacity.gtb.mvc.service.UserService;
 import java.util.List;
 import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 public class UserController {
 
     private final UserService userService;
@@ -22,7 +25,15 @@ public class UserController {
     }
 
     @GetMapping(path = "/login")
-    public User userLogin(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password){
+    public User userLogin(
+            @RequestParam(name = "username")
+            @Pattern(regexp = "^([A-Z]|[a-z]|[0-9]|_){3,10}$",
+                    message = "illegal username")
+                    String username,
+            @RequestParam(name = "password")
+            @Pattern(regexp = "^.{5,12}$",
+                    message = "the length of password should be between 5 and 12")
+                    String password) {
         return userService.userLogin(username, password);
     }
 
